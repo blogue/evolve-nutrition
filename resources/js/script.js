ffd7648889ebb83635561ea1cb6e650ef609509a
Ben Logue <benjamin.logue73@gmail.com>
2016-12-18 17:40:20 -0800

    Remove contact form and create popup modal with animation.

--- resources/js/script.js
+++ resources/js/script.js
@@ -25,6 +25,12 @@ $(document).ready(function() {
   });
 
   /* Side contact popout */
+
+  function closePopout() {
+    $('.side-contact').addClass('slideOutRight');
+    $('.side-contact').fadeOut(750);
+  }
+
   $('.js-wp-testimonial').waypoint(function(direction) {
     $('.side-contact').show('fade', 200);
     $('.side-contact').addClass('animated slideInRight');
@@ -34,8 +40,21 @@ $(document).ready(function() {
   });
 
   $('.close').click(function() {
-    $('.side-contact').addClass('slideOutRight');
-    $('.side-contact').fadeOut(750);
+    closePopout();
+  });
+
+  /* Contact Modal */
+  $('.contact-btn').click(function() {
+    closePopout();
+    $('.overlay').show('fade', 1500);
+    $('.modal').show('fade', 200);
+    $('.modal').addClass('animated bounceInUp');
+  });
+
+  $('#close-modal').click(function() {
+    $('.overlay').hide('fade', 300);
+    $('.modal').addClass('slideOutDown');
+    $('.modal').hide('fade', 1400);
   });
 
   /* Animations */
